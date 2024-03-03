d7b4ce04f8b1955c9bbcc7e6d4b27b35730f8455
Kim Hobin <isb0573@naver.com>
2024-03-03 18:04:28 +0900

    init commit

--- src/pages/calculator.tsx
+++ src/pages/calculator.tsx
@@ -2,48 +2,40 @@ import styles from '@/styles/calculator.module.scss'
 import { useState } from 'react';
 
 const Calculator = () => {
-    const [res, setRes] = useState<string>("");
+    const [res, setRes] = useState<string>(""); // 결과값
     const [cal, setCal] = useState<string>("");
-    const [tmp, setTmp] = useState<string[]>(["abc", "adsd"]);
-    const value: string[] = ["%", "CE", "C", "X", "1/x", "x2", "2/x", "\"", "7", "8", "9", "4", "5", "6", "1", "2", "3"];
+    const [tmp, setTmp] = useState<string[]>([]); // 계산
+    const sign: string[] = ["%", "CE", "C", "X", "1/x", "x2", "2/x", "\"", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "+/-", "0", ".", "="];
+    const signs = sign.map((sign) => {
+        var className;
+        if (sign === '=') {
+            className = styles.blu;
+        }
+        else {
+            className = styles.grey;
+        }
+        return (
+            <button className={className} onClick={(event) => {
+                var newTmp: string[] = [...tmp];
+                newTmp.push(sign);
+                setTmp(newTmp);
+                setRes(tmp.join(''))
+            }}>{sign}</button>
+        )
+    })
     return (
         <>
             <h1>{cal}</h1>
+            <h1>{tmp}</h1>
             <h1><a href="/">Home</a></h1>
             <div className={styles.container}>
                 <div>
-                    <input type="text" onChange={(event) => {
+                    <div className={styles.preview}><p>aa</p></div>
+                    <input className={styles.res} type="text" onChange={(event) => {
                         setCal(event.target.value);
-                    }} ></input>
-                    <div>
-                        <button className={styles.grey}>%</button>
-                        <button className={styles.grey}>CE</button>
-                        <button className={styles.grey}>C</button>
-                        <button className={styles.grey}>X</button>
-                        <button className={styles.grey}>1/x</button>
-                        <button className={styles.grey}>x2</button>
-                        <button className={styles.grey}>2/x</button>
-                        <button className={styles.grey}>\</button>
-                        <button className={styles.grey} onClick={() => {
-                            () => {
-
-                            }
-                        }}>7</button>
-                        <button className={styles.grey}>8</button>
-                        <button className={styles.grey}>9</button>
-                        <button className={styles.grey}>*</button>
-                        <button className={styles.grey}>4</button>
-                        <button className={styles.grey}>5</button>
-                        <button className={styles.grey}>6</button>
-                        <button className={styles.grey}>-</button>
-                        <button className={styles.grey}>1</button>
-                        <button className={styles.grey}>2</button>
-                        <button className={styles.grey}>3</button>
-                        <button className={styles.grey}>+</button>
-                        <button className={styles.grey}>+/-</button>
-                        <button className={styles.grey}>0</button>
-                        <button className={styles.grey}>.</button>
-                        <button className={styles.blu}>=</button>
+                    }} value={res}></input>
+                    <div className={styles.btn}>
+                        {signs}
                     </div>
                 </div>
             </div>
