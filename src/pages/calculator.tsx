6632ebc4b0fe68509b07072fabb85f68ff4a00b0
Kim Hobin <isb0573@naver.com>
2024-03-04 22:17:43 +0900

    init commit

--- src/pages/calculator.tsx
+++ src/pages/calculator.tsx
@@ -1,10 +1,11 @@
 import styles from '@/styles/calculator.module.scss'
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 const Calculator = () => {
-    const [res, setRes] = useState<string>(""); // 결과값
-    const [cal, setCal] = useState<string>("");
     const [tmp, setTmp] = useState<string[]>([]); // 계산
+    const [res, setRes] = useState<string>(""); // 결과
+    const [cal, setCal] = useState<string>("");
+    const flag = useRef(Boolean);
     const sign: string[] = ["%", "CE", "C", "X", "1/x", "x2", "2/x", "\"", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "+/-", "0", ".", "="];
     const signs = sign.map((sign) => {
         var className;
@@ -16,10 +17,16 @@ const Calculator = () => {
         }
         return (
             <button className={className} onClick={(event) => {
-                var newTmp: string[] = [...tmp];
-                newTmp.push(sign);
-                setTmp(newTmp);
-                setRes(tmp.join(''))
+                if (sign === '+') {
+                    if (flag) {
+                    }
+                    else {
+
+                    }
+                }
+                else {
+                    setCal(cal + sign);
+                }
             }}>{sign}</button>
         )
     })
@@ -33,7 +40,7 @@ const Calculator = () => {
                     <div className={styles.preview}><p>aa</p></div>
                     <input className={styles.res} type="text" onChange={(event) => {
                         setCal(event.target.value);
-                    }} value={res}></input>
+                    }} value={cal}></input>
                     <div className={styles.btn}>
                         {signs}
                     </div>
