eac2cc566fb1ce0ff6ffecba3f905d9351a2dc27
Kim Hobin <isb0573@naver.com>
2024-03-05 21:20:29 +0900

    init commit

--- src/pages/calculator.tsx
+++ src/pages/calculator.tsx
@@ -2,13 +2,17 @@ import styles from '@/styles/calculator.module.scss'
 import { useRef, useState } from 'react';
 
 const Calculator = () => {
-    const [tmp, setTmp] = useState<string[]>([]); // 계산
-    const [res, setRes] = useState<string>(""); // 결과
-    const [cal, setCal] = useState<string>("");
-    const flag = useRef(Boolean);
     const sign: string[] = ["%", "CE", "C", "X", "1/x", "x2", "2/x", "\"", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "+/-", "0", ".", "="];
+    const [tmp, setTmp] = useState<string>(""); // 계산
+    const [res, setRes] = useState<string>(""); // 결과
+    const temp = useRef<string>(""); // 입력값
+    const preNum = useRef<string>(""); // 첫번째 값
+    const nextNum = useRef<string>(""); // 두번째 값 
+    const operator = useRef<string>(""); // 기존 연산자 유무
+    const addCal = useRef<boolean>(false); // 추가 계산 유무
     const signs = sign.map((sign) => {
         var className;
+
         if (sign === '=') {
             className = styles.blu;
         }
@@ -17,30 +21,44 @@ const Calculator = () => {
         }
         return (
             <button className={className} onClick={(event) => {
-                if (sign === '+') {
-                    if (flag) {
+                if (operator.current) { // 기존 연산자 존재
+                    if (sign === '=') { // 연산자 입력 시
+                        setTmp(tmp + sign);
+                        setRes(String(Number(preNum.current) + Number(nextNum.current)));
+                        preNum.current = String(Number(preNum.current) + Number(nextNum.current));
+                        nextNum.current = "";
+                        operator.current = "";
                     }
                     else {
-
+                        setTmp(tmp + sign);
+                        nextNum.current = nextNum.current + sign;
+                        setRes(nextNum.current);
                     }
                 }
-                else {
-                    setCal(cal + sign);
+                else { // preNum 계산
+                    if (sign == '+') { // 연산자 입력 시
+                        setTmp(preNum.current + sign);
+                        operator.current = "+";
+                    }
+                    else {
+                        preNum.current = preNum.current + sign;
+                        setRes(preNum.current);
+                        setTmp(preNum.current);
+                    }
+
                 }
-            }}>{sign}</button>
+            }}> {sign}</button >
         )
     })
     return (
         <>
-            <h1>{cal}</h1>
-            <h1>{tmp}</h1>
             <h1><a href="/">Home</a></h1>
             <div className={styles.container}>
                 <div>
-                    <div className={styles.preview}><p>aa</p></div>
+                    <div className={styles.preview}><p>{tmp}</p></div>
                     <input className={styles.res} type="text" onChange={(event) => {
-                        setCal(event.target.value);
-                    }} value={cal}></input>
+                        setRes(event.target.value);
+                    }} value={res}></input>
                     <div className={styles.btn}>
                         {signs}
                     </div>
