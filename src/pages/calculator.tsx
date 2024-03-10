3fb71cdfd74d9645116bc77214df941a73cae296
Kim Hobin <isb0573@naver.com>
2024-03-10 22:21:44 +0900

    == 이슈 수정

--- src/pages/calculator.tsx
+++ src/pages/calculator.tsx
@@ -45,111 +45,19 @@ const Calculator = () => {
         key={idx}
         className={sign === "=" ? styles.blu : styles.grey}
         onClick={() => {
-          // if (sign === "X") {
-          //     addCal.current = false;
-          //     preNum.current = "";
-          //     nextNum.current = "";
-          //     operator.current = "";
-          //     setRes("0");
-          //     setTmp("");
-          //   } else if (operator.current) {
-          //     // 기존 연산자 존재
-          //     console.log("++ operator ++");
-          //     if (sign === "=") {
-          //       // '=' 입력
-          //       console.log("=");
-          //       setTmp(tmp + sign);
-          //       if (tmp[tmp.length - 1] === "=") {
-          //         // ex) ==
-          //         console.log("aa");
-          //         setTmp(
-          //           preNum.current + operator.current + nextNum.current + "="
-          //         );
-          //         preNum.current = String(
-          //           Number(preNum.current) + Number(nextNum.current)
-          //         );
-          //         setRes(preNum.current);
-          //       } else if (tmp[tmp.length - 1] === operator.current) {
-          //         // ex) ++ --
-          //         nextNum.current = preNum.current;
-          //         setTmp(
-          //           preNum.current + operator.current + nextNum.current + "="
-          //         );
-          //         preNum.current = String(
-          //           Number(preNum.current) + Number(nextNum.current)
-          //         );
-          //         setRes(preNum.current);
-          //       } else if (operator.current === "+") {
-          //         console.log("---");
-          //         preNum.current = String(
-          //           Number(preNum.current) + Number(nextNum.current)
-          //         );
-          //         setRes(preNum.current);
-          //       } else if (operator.current === "-") {
-          //         setRes(
-          //           String(Number(preNum.current) - Number(nextNum.current))
-          //         );
-          //         preNum.current = String(
-          //           Number(preNum.current) - Number(nextNum.current)
-          //         );
-          //       } else if (operator.current === "*") {
-          //         setRes(
-          //           String(Number(preNum.current) * Number(nextNum.current))
-          //         );
-          //         preNum.current = String(
-          //           Number(preNum.current) * Number(nextNum.current)
-          //         );
-          //       } else if (operator.current === "/") {
-          //         setRes(
-          //           String(Number(preNum.current) / Number(nextNum.current))
-          //         );
-          //         preNum.current = String(
-          //           Number(preNum.current) / Number(nextNum.current)
-          //         );
-          //       }
-          if (operator.current) {
-            // 기존 연산자 존재
-            console.log("++ operator ++");
-            if (sign === "=") {
-              // '=' 입력
-              console.log("=");
-              setTmp(tmp + sign);
-              const result = operate(
-                Number(preNum.current),
-                Number(nextNum.current),
-                operator.current
-              );
-              setRes(String(result));
-              preNum.current = String(result);
-              nextNum.current = "";
-              operator.current = null;
-            } else if (sign === "+") {
-              // 추가 연산
-              console.log("++ 연산x2 ++");
-              const result = operate(
-                Number(preNum.current),
-                Number(nextNum.current),
-                operator.current
-              );
-              setTmp(String(result) + sign);
-              setRes(String(result));
-              preNum.current = String(result);
-              nextNum.current = "";
-              addCal.current = true;
-            } else {
-              if (addCal.current) {
-                console.log("++ 추가 연산 ++");
-                addCal.current = false;
-              } else {
-                console.log("++ nextNum 계산 ++");
-              }
-              nextNum.current = nextNum.current + sign;
-              setTmp(tmp + sign);
-              setRes(nextNum.current);
-            }
-          } else {
-            // preNum 계산
+          if (sign === "X") {
+            // @@@@ if문 쓰는 방식?
+            preNum.current = "";
+            nextNum.current = "";
+            operator.current = null;
+            setRes("0");
+            setTmp("");
+          } else if (sign === "=" && res === "0") {
+            setTmp("0=");
+          } else if (!operator.current) {
+            // preNum 계산, 첫번째 계산
             if (sign === "+" || sign === "-" || sign === "*" || sign === "/") {
+              // @@@@ type ??
               // 연산자 입력 시
               console.log("+");
               if (!preNum.current) {
@@ -163,6 +71,61 @@ const Calculator = () => {
               preNum.current = preNum.current + sign;
               setRes(preNum.current);
               setTmp(preNum.current);
+            }
+          } else {
+            // 기존 연산자 존재
+            console.log("++ operator ++");
+            if (sign === "=") {
+              setTmp(tmp + sign);
+              if (tmp[tmp.length - 1] === "=") {
+                // sign : ==
+                console.log("==");
+                const result = operate(
+                  Number(preNum.current),
+                  Number(nextNum.current),
+                  operator.current
+                );
+                setTmp(
+                  preNum.current + operator.current + nextNum.current + "="
+                );
+                preNum.current = String(result);
+                setRes(preNum.current);
+              } else {
+                console.log("== 아닐 때");
+                setTmp(tmp + sign);
+                const result = operate(
+                  Number(preNum.current),
+                  Number(nextNum.current),
+                  operator.current
+                );
+                setRes(String(result));
+                preNum.current = String(result);
+              }
+            } else if (sign === "+") {
+              // 추가 연산 N + N +
+              console.log("++ 연산x2 ++");
+              const result = operate(
+                Number(preNum.current),
+                Number(nextNum.current),
+                operator.current
+              );
+              setTmp(String(result) + sign);
+              setRes(String(result));
+              preNum.current = String(result);
+              nextNum.current = "";
+              addCal.current = true;
+            } else {
+              if (!addCal.current) {
+                // N + N
+                console.log("++ nextNum 계산 ++");
+              } else {
+                // N + N + N
+                console.log("++ 추가 연산 ++");
+                addCal.current = false;
+              }
+              nextNum.current = nextNum.current + sign;
+              setTmp(tmp + sign);
+              setRes(nextNum.current);
             }
           }
         }}
